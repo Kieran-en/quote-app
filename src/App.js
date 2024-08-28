942953a1f2db6c90e530a2ca1ecb6e2e5540c3ea
Enzo Kieran <enzokamtchouang@gmail.com>
2024-08-28 13:55:14 +0100

    making code prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import { RandomQuote } from './Components/RandomQuote/RandomQuote';
-import React, {useState} from 'react'
+import "./App.css";
+import { RandomQuote } from "./Components/RandomQuote/RandomQuote";
+import React, { useState } from "react";
 
 function App() {
-
-  const [currentBackgroundColor, setCurrentBackgroundColor] = useState(["#38858a"]);
+  const [currentBackgroundColor, setCurrentBackgroundColor] = useState([
+    "#38858a",
+  ]);
 
   const backgroundColor = ["#ba4949", "#38858a", "#397097", "#3f384c"];
 
@@ -12,14 +13,19 @@ function App() {
     const color = ["#c15c5c", "#4c9196", "#4d7fa2", "#3f3434"];
     const select = color[Math.floor(Math.random() * color.length)];
     setCurrentBackgroundColor(select);
-
-}
+  };
 
   return (
-    <div className='whole' style={{
-      backgroundColor: currentBackgroundColor 
-  }}>
-      <RandomQuote currentBackgroundColor={currentBackgroundColor} changeColor={changeColor} />
+    <div
+      className="whole"
+      style={{
+        backgroundColor: currentBackgroundColor,
+      }}
+    >
+      <RandomQuote
+        currentBackgroundColor={currentBackgroundColor}
+        changeColor={changeColor}
+      />
     </div>
   );
 }
